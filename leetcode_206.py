2a3f30a12cd92da8ac0db56b34e884b0d21cfef4
gmdzy2010 <gmdzy2010@126.com>
2021-02-13 21:23:47 +0800

    20210213-206题，递归法

--- leetcode_206.py
+++ leetcode_206.py
@@ -1,7 +1,11 @@
 class ListNode:
+    
     def __init__(self, value=0, next_node=None):
         self.value = value
         self.next_node = next_node
+    
+    def reverse(self, test):
+        return self.reverse(test)
 
 
 def reverse_linked_list_direct(head):
@@ -33,6 +37,18 @@ def reverse_linked_list_double_pointer(head):
     return previous
 
 
+def reverse(current, previous):
+    if current is None:
+        return previous
+    new_head = current.next_node
+    current.next_node = previous
+    return reverse(new_head, current)
+
+
+def reverse_linked_list_recursion(head):
+    return reverse(head, None)
+
+
 if __name__ == '__main__':
     # Create a linked-list.
     test_head = test_current = ListNode()
@@ -41,7 +57,8 @@ if __name__ == '__main__':
         test_current = test_current.next_node
     
     # test_new_head = reverse_linked_list_direct(test_head.next)
-    test_new_head = reverse_linked_list_double_pointer(test_head.next_node)
+    # test_new_head = reverse_linked_list_double_pointer(test_head.next_node)
+    test_new_head = reverse_linked_list_recursion(test_head.next_node)
     while test_new_head:
         print(test_new_head.value)
         test_new_head = test_new_head.next_node
