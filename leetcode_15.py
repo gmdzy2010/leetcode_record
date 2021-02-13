78294ce7e4b3f1a627924ee49ff1e63db37d37ad
gmdzy2010 <gmdzy2010@126.com>
2021-02-13 15:15:51 +0800

    20210213-15题，双指针法优化

--- leetcode_15.py
+++ leetcode_15.py
@@ -1,54 +1,47 @@
-def three_number_sum_hash(nums):
-    index_1, length, result = 0, len(nums), []
-    nums.sort()
-    # position = {}
-    while index_1 < length:
-        if nums[index_1] > 0:
-            index_1 += 1
-            continue
-        elif index_1 > 0 and nums[index_1] == nums[index_1 - 1]:
-            index_1 += 1
-            continue
-        
-        # use dict to record position.
-        index_2 = index_1 + 1
-        position = {}
-        while index_2 < length:
-            if index_2 > index_1 + 2 \
-                    and nums[index_2] == nums[index_2 - 1] \
-                    and nums[index_2 - 1] == nums[index_2 - 2]:
-                continue
-            third_num = - (nums[index_1] + nums[index_2])
-            if third_num not in position:
-                result.append(sorted([nums[index_1], nums[index_2], third_num]))
-            else:
-                position[third_num] = - (nums[index_1] + nums[index_2])
-            index_2 += 1
-        index_1 += 1
-    return result
-
-
 def three_number_sum_double_pointer(sequence):
     length, result = len(sequence), []
+    
+    # It's necessary to inplace sort the original sequence to use the
+    # double-pointer scan way.
     sequence.sort()
-    for index_1 in range(length):
-        if index_1 > 0 and sequence[index_1] == sequence[index_1 - 1]:
+    
+    # Start scan use left, middle and right index.
+    for left_i in range(length):
+        # Stop the left index loop when the most left element of the sorted
+        # sequence is larger than 0.
+        if sequence[left_i] > 0:
+            break
+        
+        # Duplicate removal.
+        if left_i > 0 and sequence[left_i] == sequence[left_i - 1]:
             continue
-        index_3 = length - 1
-        target = -sequence[index_1]
-        for index_2 in range(index_1 + 1, length):
-            if index_2 > index_1 + 1 and sequence[index_2] == sequence[index_2 - 1]:
+        
+        right_i, target = length - 1, -sequence[left_i]
+        for middle_i in range(left_i + 1, length):
+            # Combined with code of line 5 to deal with the situation of
+            # continuously appear the same element.
+            if middle_i > left_i + 1 \
+                    and sequence[middle_i] == sequence[middle_i - 1]:
                 continue
-            while index_2 < index_3 and sequence[index_2] + sequence[index_3] > target:
-                index_3 -= 1
-            if index_2 == index_3:
+            
+            # Retreat right index if sum of elements on the middle index and
+            # right index is larger than the one on the left index.
+            while middle_i < right_i \
+                    and sequence[middle_i] + sequence[right_i] > target:
+                right_i -= 1
+            
+            # stop the for loop when middle index meets the right index.
+            if middle_i >= right_i:
                 break
-            if sequence[index_2] + sequence[index_3] == target:
-                result.append([sequence[index_1], sequence[index_2], sequence[index_3]])
+            
+            # Store answer.
+            if sequence[middle_i] + sequence[right_i] == target:
+                result.append(
+                    [sequence[left_i], sequence[middle_i], sequence[right_i]])
     return result
 
 
 if __name__ == '__main__':
-    test_sequence = [-1, 0, 1, 2, -1, -4]
-    test_result = three_number_sum_hash(test_sequence)
+    test_sequence = [-1, 0, 1, 2, 2, 2, 2, -1, -1, -4, 3, 4, 7]
+    test_result = three_number_sum_double_pointer(test_sequence)
     print(test_result)
