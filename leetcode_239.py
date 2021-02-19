4d9b6b90040db4cceb721e8737d111f8a25a14e5
gmdzy2010 <gmdzy2010@126.com>
2021-02-19 16:25:29 +0800

    20210219-239题，单调队列法加注释

--- leetcode_239.py
+++ leetcode_239.py
@@ -1,34 +1,46 @@
 from collections import deque
 
 
-class MonoQuene:
-    """A simple mono-quene implementation."""
-    
-    def __init__(self):
-        self._quene = deque()
-    
-    def push(self, value):
-        while self._quene and value > self._quene[0]:
-            self._quene.popleft()
-        self._quene.appendleft(value)
-        
-    def pop(self, value):
-        if self._quene and self._quene[-1] == value:
-            self._quene.pop()
-    
-    def front(self):
-        return self._quene[-1]
-
-
 def max_sliding_window_direct(sequence, k):
     return [max(sequence[i:i + k]) for i in range(len(sequence) - k + 1)]
 
 
+class MonoQuene:
+    """A simple mono-quene implementation."""
+    
+    def __init__(self):
+        self.quene = deque()
+    
+    def push(self, value):
+        # the front of the mono-quene should always be the largest one.
+        # So all of the elements smaller than the pushed value should be pop
+        # out on the left.
+        while self.quene and value > self.quene[0]:
+            self.quene.popleft()
+        self.quene.appendleft(value)
+    
+    def pop(self, value):
+        # The quene just need pop out the element which value equals to the
+        # last element removed from the sliding window.
+        if self.quene and self.quene[-1] == value:
+            self.quene.pop()
+    
+    def front(self):
+        return self.quene[-1]
+    
+    def __str__(self):
+        return str(self.quene)
+
+
 def max_sliding_window_quene(sequence, k):
     quene, result = MonoQuene(), list()
+    
+    # Initialize the quene
     for index in range(k):
         quene.push(sequence[index])
     result.append(quene.front())
+    
+    # the window should start to slide at index k.
     for index in range(k, len(sequence)):
         quene.pop(sequence[index - k])
         quene.push(sequence[index])
@@ -37,6 +49,6 @@ def max_sliding_window_quene(sequence, k):
 
 
 if __name__ == '__main__':
-    test_sequence, test_k = [1, 3, -1, -3, 5, 3, 6, 7], 3
+    test_sequence, test_k = [1, 3, -1, -3, 5, 3, 3, 6, 7], 3
     print(max_sliding_window_direct(test_sequence, test_k))
     print(max_sliding_window_quene(test_sequence, test_k))
