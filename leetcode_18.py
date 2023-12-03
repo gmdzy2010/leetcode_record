2a423176b1a852604e168b1bcac14a1ff93c29e0
gmdzy2010 <gmdzy2010@126.com>
2023-12-03 15:23:28 +0800

    chore(old): collect old code to old

--- leetcode_18.py
+++
@@ -1,9 +0,0 @@
-def four_number_sum_double_pointer(sequence, target):
-    result = []
-    return result
-
-
-if __name__ == '__main__':
-    test_sequence, test_target = [1, 0, -1, 0, -2, 2], 0
-    test_result = four_number_sum_double_pointer(test_sequence, test_target)
-    print(test_result)
