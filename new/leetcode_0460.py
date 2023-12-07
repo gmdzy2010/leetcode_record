a07aba82fded94d78f1d277def226c1369b942b9
gmdzy2010 <gmdzy2010@126.com>
2023-12-07 18:51:51 +0800

    feat(new): 新提交的代码

---
+++ new/leetcode_0460.py
@@ -0,0 +1,11 @@
+class LFUCache:
+    """最不经常使用()缓存算法简单实现"""
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+
+    def get(self, key: int) -> int:
+        return -1
+
+    def put(self, key: int, value: int):
+        return None
