9a2c39615cad8a765a095731518e00fff8709cd9
Yang Li <gmdzy2010@126.com>
2023-12-13 12:15:37 +0800

    chore(new): 两数之和更新注释

--- new/leetcode_0001.py
+++ new/leetcode_0001.py
@@ -2,23 +2,27 @@ from typing import List
 
 
 def main(nums: List[int], target: int) -> List[int]:
-    """求两数之和
+    """两数之和
 
     Args:
-        nums (List[int]): 原数组
-        target (int): 两数之和的目标值
+        - nums (List[int]): 原数组
+        - target (int): 两数之和的目标值
 
     Returns:
-        List[int]: 符合条件的两个数下标
+        - List[int]: 符合条件的两个数下标
     """
     size = len(nums)
+
+    # * 用哈希表记录遍历过的元素 -> key，及这个元素在的位置 -> value
     record = {}
 
     for i in range(size):
         value = target - nums[i]
+        # * 目标值已经出现过了，直接从哈希表里面取出来
         if value in record:
             return [i, record[value]]
 
+        # * 不在哈希表里面的元素就记下来这个元素以及位置
         record[nums[i]] = i
     return []
 
