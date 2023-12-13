c08ebd6eec34448d3219527c0dd52b307a0a5b83
gmdzy2010 <gmdzy2010@126.com>
2023-12-13 18:38:51 +0800

    chore(new): 二叉树的zigzag层序遍历，新增第二种办法

--- new/leetcode_0103.py
+++ new/leetcode_0103.py
@@ -16,10 +16,52 @@ class TreeNode:
         self.right: Self | None = right
 
 
-def tranverse_in_zigzag_levelorder(root: TreeNode | None) -> List[List[int]]:
+def tranverse_in_zigzag_levelorder_1(root: TreeNode | None) -> List[List[int]]:
     """二叉树的zigzag层序遍历
 
-    从二叉树的层序遍历修改即可
+    从二叉树的层序遍历修改
+    - 使用双端队列保存每层的节点的值
+        - 当层数是偶数时，节点的值从队列尾部(appendleft)入队
+        - 当层数是奇数时，节点的值从队列头部(append)入队
+
+    Args:
+        - root (TreeNode | None): 二叉树根节点
+
+    Returns:
+        - List[List[int]]: zigzag层序遍历的结果
+    """
+    ans: List[List[int]] = []
+    if not root:
+        return ans
+
+    level: int = 0
+    nodes: Deque[TreeNode] = deque()
+    nodes.appendleft(root)
+    while nodes:
+        size = len(nodes)
+        node_values: Deque[int] = deque()
+        for _ in range(size):
+            node = nodes.pop()
+            if level % 2 == 0:
+                node_values.append(node.val)
+            else:
+                node_values.appendleft(node.val)
+
+            if node.left:
+                nodes.appendleft(node.left)
+            if node.right:
+                nodes.appendleft(node.right)
+
+        ans.append(list(node_values))
+        level += 1
+
+    return ans
+
+
+def tranverse_in_zigzag_levelorder_2(root: TreeNode | None) -> List[List[int]]:
+    """二叉树的zigzag层序遍历
+
+    从二叉树的层序遍历，直接反转每层的列表即可
 
     Args:
         - root (TreeNode | None): 二叉树根节点
@@ -49,3 +91,13 @@ def tranverse_in_zigzag_levelorder(root: TreeNode | None) -> List[List[int]]:
         level += 1
 
     return ans
+
+
+if __name__ == "__main__":
+    test_root = TreeNode(1)
+    test_root.left = TreeNode(2)
+    test_root.right = TreeNode(3)
+    test_root.left.left = TreeNode(4)
+    test_root.right.right = TreeNode(5)
+    print(tranverse_in_zigzag_levelorder_1(test_root))
+    print(tranverse_in_zigzag_levelorder_2(test_root))
