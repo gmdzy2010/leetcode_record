0a4456cf108c12f186858d3979881dd86d7341c5
gmdzy2010 <gmdzy2010@126.com>
2023-12-13 17:06:23 +0800

    feat(new): 二叉树右视图，BFS简单修改即可

---
+++ new/leetcode_0199.py
@@ -0,0 +1,50 @@
+from collections import deque
+from typing import Deque, List, Self
+
+
+class TreeNode:
+    """二叉树节点"""
+
+    def __init__(
+        self,
+        val: int = 0,
+        left: Self | None = None,
+        right: Self | None = None,
+    ):
+        self.val = val
+        self.left: Self | None = left
+        self.right: Self | None = right
+
+
+def right_view_of_binary_tree(root: TreeNode | None) -> List[int]:
+    """二叉树的右视图
+
+    使用二叉树层序遍历改造一下即可
+
+    Args:
+        - root (TreeNode | None): 二叉树根节点
+
+    Returns:
+        - List[int]: 层序遍历最右边的值列表
+    """
+    ans: List[int] = []
+    if not root:
+        return ans
+
+    nodes: Deque[TreeNode] = deque()
+    nodes.appendleft(root)
+    while nodes:
+        size = len(nodes)
+        node_values: List[int] = []
+
+        for _ in range(size):
+            node = nodes.pop()
+            node_values.append(node.val)
+            if node.left:
+                nodes.appendleft(node.left)
+            if node.right:
+                nodes.appendleft(node.right)
+
+        ans.append(node_values[-1])
+
+    return ans
