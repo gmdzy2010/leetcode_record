fb0e3948fa4c6344d3014bf242ca4fd991846f98
gmdzy2010 <gmdzy2010@126.com>
2024-01-06 17:21:42 +0800

    feat(new): 数字组合，回溯

---
+++ new/leetcode_0077.py
@@ -0,0 +1,62 @@
+from typing import List
+
+
+def main(n: int, k: int) -> List[List[int]]:
+    """数字组合
+
+    - 回溯法
+    - 回溯法 + 减枝
+
+    Args:
+        - n (int): 不同数字个数
+        - k (int): 组合包含数字个数
+
+    Returns:
+        - List[List[int]]: 所有组合结果列表
+    """
+    ans: List[List[int]] = []
+
+    # * 保留每个组合的结果
+    path: List[int] = []
+
+    backtracking(n, k, 1, path, ans)
+
+    return ans
+
+
+def backtracking(
+    n: int,
+    k: int,
+    start: int,
+    path: List[int],
+    ans: List[List[int]],
+):
+    """_summary_
+
+    Args:
+        - n (int): 不同数字个数
+        - k (int): 组合包含数字个数
+        - start (int): 组合开始的数字
+        - path (List[int]): 满足条件的组合
+        - ans (List[List[int]]): 结果
+    """
+    if len(path) == k:
+        # * 组合中的数字个数达到 k 返回即可
+        # ! 注意这里是切片复制
+        ans.append(path[:])
+
+        return
+
+    for i in range(start, n + 1):
+        # * 本轮回溯的数字填进去
+        path.append(i)
+
+        # * 下一轮回溯将下一个位置的数填进去
+        backtracking(n, k, i + 1, path, ans)
+
+        # * 数字填进去呢要记得再弹出来，否则下一轮循环将会多一个数字在结果中
+        path.pop()
+
+
+if __name__ == "__main__":
+    main(5, 2)
