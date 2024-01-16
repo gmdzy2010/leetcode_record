8e92635d1e6dc1625d5c2a81b0a0ed98c48ddd24
gmdzy2010 <gmdzy2010@126.com>
2024-01-16 13:09:41 +0800

    chore(new): 爬楼梯扩展，DP

--- new/leetcode_0070.py
+++ new/leetcode_0070.py
@@ -24,5 +24,33 @@ def main(n: int) -> int:
     return dp[n]
 
 
+def main_2(n: int) -> int:
+    """爬特定楼梯
+
+    扩展：7的倍数台阶不能跳
+
+    Args:
+        - n (int): 数字
+
+    Returns:
+        - int: 总共的方案
+    """
+    if n < 2:
+        return n
+
+    # * dp[i] 代表跳跃 n 个台阶的方案数量
+    # ! 因为 0 没有意义，所以 dp 数组多一个元素
+    dp = [0] * (n + 1)
+
+    # * 初始化没有dp[0]，其没有意义
+    dp[1] = 1
+    dp[2] = 2
+
+    for i in range(3, n + 1):
+        dp[i] = dp[i - 2] + dp[i - 1] if i % 7 != 0 else 0
+
+    return dp[n]
+
+
 if __name__ == "__main__":
     print(main(7))
