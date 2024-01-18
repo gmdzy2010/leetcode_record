55b9d09921833d759a9ae5f126e3f803b378ec39
gmdzy2010 <gmdzy2010@126.com>
2024-01-18 14:37:59 +0800

    feat(new): 完全平方数，DP-完全背包

---
+++ new/leetcode_0279.py
@@ -0,0 +1,29 @@
+from math import sqrt
+
+
+def main(n: int) -> int | float:
+    """完全平方数，DP-完全背包
+
+    Args:
+        - n (int): 目标数字
+
+    Returns:
+        - int | float: 得到 n 需要的最少平方数
+    """
+    # * dp[i] 代表凑成总额为 i 的最少金币数量
+    dp = [float("inf")] * (n + 1)
+
+    dp[0] = 0
+
+    # * 物品：平方小于 n 的所有数字，可以任意选择
+    for i in range(1, int(sqrt(n)) + 1):
+        x = i * i
+        for j in range(x, n + 1):
+            if dp[j - x] != float("inf"):
+                dp[j] = min(dp[j], dp[j - x] + 1)
+
+    return -1 if dp[-1] == float("inf") else dp[-1]
+
+
+if __name__ == "__main__":
+    print(main(9))
