84b81706480727860bf4f528af78ed0496f241a8
gmdzy2010 <gmdzy2010@126.com>
2024-01-19 14:26:15 +0800

    chore(new): 卖股票2，DP-二维

--- new/leetcode_0122.py
+++ new/leetcode_0122.py
@@ -1,8 +1,21 @@
 from typing import List
 
 
-def main(prices: List[int]) -> int:
+def main(prices: List[int]):
     """股票的最大利润
+
+    Args:
+        - prices (List[int]): 价格列表
+    """
+    ans1 = max_profit_greedy(prices)
+    print(ans1)
+
+    ans2 = max_profit_dp_2(prices)
+    print(ans2)
+
+
+def max_profit_greedy(prices: List[int]) -> int:
+    """股票的最大利润，贪心
 
     Args:
         - prices (List[int]): 价格列表
@@ -21,6 +34,41 @@ def main(prices: List[int]) -> int:
         max_profit += max(profit, 0)
 
     return max_profit
+
+
+def max_profit_dp_2(prices: List[int]) -> int | float:
+    """买卖股票的最佳时机2，DP-二维数组
+
+    Args:
+        - prices (List[int]): 每日价格数组
+
+    Returns:
+        - int: 最大利润
+    """
+    size = len(prices)
+    if not size:
+        return 0
+
+    # * dp[i] 当天有两个状态，持有和不持有
+    # * dp[i][0]/dp[i][1] 代表第 i 天持有/不持有股票拥有的最多现金
+    dp = [[0 for _ in range(2)] for _ in range(size)]
+
+    # * 第 0 天持有股票，只能是当天买了股票
+    dp[0][0] = 0 - prices[0]
+    dp[0][1] = 0
+
+    for i in range(1, size):
+        # * 第 i 天持有股票有两种可能，两者取现金多的情况：
+        # * 1. 第 i-1 天就持有股票 -> dp[i - 1][0]
+        # * 2. 第 i-1 天不持有股票 -> dp[i - 1][1] ，第 i 天买入股票 -> - prices[i]
+        dp[i][0] = max(dp[i - 1][0], dp[i - 1][1] - prices[i])
+
+        # * 第 i 天不持有股票也有两种可能性，两者取现金多的情况：
+        # * 1. 第 i-1 天就不持有股票
+        # * 2. 第 i 天卖出操作导致当天不持有股票，此时手上现金增加了 price[i]
+        dp[i][1] = max(dp[i - 1][1], prices[i] + dp[i - 1][0])
+
+    return dp[-1][1]
 
 
 if __name__ == "__main__":
