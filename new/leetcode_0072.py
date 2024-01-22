35829df2d3b49916db57cd0a7fdffaf87ff27b7c
gmdzy2010 <gmdzy2010@126.com>
2024-01-22 19:25:42 +0800

    feat(new): 编辑距离，DP-二维

---
+++ new/leetcode_0072.py
@@ -0,0 +1,45 @@
+def main(word1: str, word2: str) -> int:
+    """编辑距离，DP-二维
+
+    Args:
+        - word1 (str): 字符串1
+        - word2 (str): 字符串2
+
+    Returns:
+        - int: 最少的操作
+    """
+    size_1, size_2 = len(word1), len(word2)
+
+    # * dp[i][j] 代表让以 word1[i-1]/word2[j-1] 结尾的两个字符串编辑距离
+    dp = [[0 for _ in range(size_2 + 1)] for _ in range(size_1 + 1)]
+
+    # * dp[i][0] 代表 word2 是空字符串
+    for i in range(1, size_1 + 1):
+        dp[i][0] = i
+
+    # * dp[0][j] 同理
+    for j in range(1, size_2 + 1):
+        dp[0][j] = j
+
+    for i in range(1, size_1 + 1):
+        for j in range(1, size_2 + 1):
+            # * word1[i - 1] 等于 word2[j - 1]，此时不需要操作
+            if word1[i - 1] == word2[j - 1]:
+                dp[i][j] = dp[i - 1][j - 1]
+
+            # * word1[i - 1] 不等于 word2[j - 1]，此时有 2 种选择：
+            # * 1. 删除/增加其中一个字符， 操作数是 1
+            # * 2. 替换其中一个字符，操作数是 1
+            else:
+                dp[i][j] = min(
+                    dp[i - 1][j] + 1,
+                    dp[i][j - 1] + 1,
+                    dp[i - 1][j - 1] + 1,
+                )
+
+    return dp[-1][-1]
+
+
+if __name__ == "__main__":
+    test_str1, test_str2 = "sea", "sae"
+    print(main(test_str1, test_str2))
