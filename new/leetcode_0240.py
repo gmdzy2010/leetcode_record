55304f5a0ecf0e6353535b6ef22ab0913c251bbd
gmdzy2010 <gmdzy2010@126.com>
2024-01-24 17:31:41 +0800

    feat(new): 搜索二维矩阵，双指针

---
+++ new/leetcode_0240.py
@@ -0,0 +1,37 @@
+from typing import List
+
+
+def main(matrix: List[List[int]], target: int) -> bool:
+    """搜索二维矩阵
+
+    Args:
+        - matrix (List[List[int]]): 二维矩阵
+        - target (int): 待搜索的目标值
+
+    Returns:
+        - bool: 是否存在 target
+    """
+    size_row, size_col = len(matrix), len(matrix[0])
+
+    # * 从右上角往左下角看是一颗不严格的BST
+    i, j = size_row - 1, 0
+    while i >= 0 and j < size_col:
+        if matrix[i][j] > target:
+            i -= 1
+        elif matrix[i][j] < target:
+            j += 1
+        else:
+            return True
+
+    return False
+
+
+if __name__ == "__main__":
+    test_nums = [
+        [1, 4, 7, 11, 15],
+        [2, 5, 8, 12, 19],
+        [3, 6, 9, 16, 22],
+        [10, 13, 14, 17, 24],
+        [18, 21, 23, 26, 30],
+    ]
+    print(main(test_nums, 20))
