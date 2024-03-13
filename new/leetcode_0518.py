e0231c49e4d4ff0f9f7a2ba8097035cf8ab9e00a
gmdzy2010 <gmdzy2010@126.com>
2024-03-13 20:14:03 +0800

    chore(new): 零钱兑换，更新注释

--- new/leetcode_0518.py
+++ new/leetcode_0518.py
@@ -14,13 +14,18 @@ def main(amount: int, coins: List[int]) -> int:
     # * dp[i] 代表凑成总额为 i 的硬币组合数量
     dp = [0] * (amount + 1)
 
-    # ? 凑成总额为 0 硬币有一种方案？？？他妈的在扯淡吧？
+    # ! 当不选取任何硬币时，金额之和为 0，只有 1 种硬币组合
     dp[0] = 1
 
     # * 先物品再背包
+    # 举个例子
+    # 1. 上一轮用硬币面额 1 凑出金额 5，方案数量 dp[5]
+    # 2. 这一轮用硬币面额 2 凑出金额 5，相当于凑出金额 3 的基础上加一个面额 2 的硬币就能凑
+    #    成金额 5，即此种方案数量为 dp[3]
+    # 3. 最终得到最新的、能够凑出金额 5 的方案总数为 dp[5] + dp[3]
     for coin in coins:
-        for j in range(coin, amount + 1):
-            dp[j] += dp[j - coin]
+        for i in range(coin, amount + 1):
+            dp[i] = dp[i] + dp[i - coin]
 
     return dp[-1]
 
