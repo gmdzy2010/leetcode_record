3b3caf505d8be063489d3c5b4930e064f195b51c
gmdzy2010 <gmdzy2010@126.com>
2024-04-29 00:05:47 +0800

    chore(new): 和为 K 的子数组，更新注释

--- new/leetcode_0560.py
+++ new/leetcode_0560.py
@@ -14,10 +14,15 @@ def main(nums: List[int], k: int) -> int:
     ans, pre = 0, 0
     cnt = {0: 1}
     for n in nums:
+        # * 计算每个位置的前缀和
         pre += n
+
+        # * 说明从某个位置到当前位置的连续子数组的和为k
+        # ! 到 i 位置的 前缀和 pre，连续子数组和为 k 的次数 == pre - k 出现的次数
         if pre - k in cnt:
             ans += cnt[pre - k]
 
+        # * 哈希表存储每个前缀和出现的次数
         if pre in cnt:
             cnt[pre] += 1
         else:
